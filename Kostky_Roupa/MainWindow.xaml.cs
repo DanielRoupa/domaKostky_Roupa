13d7656fbb6bfb6876d0db714f49626770ef84ea
Daniel Roupa <Roupa.Daniel@stud.vassboskovice.cz>
2024-06-06 17:59:17 +0200

    konec

--- Kostky_Roupa/MainWindow.xaml.cs
+++ Kostky_Roupa/MainWindow.xaml.cs
@@ -1,41 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace Kostky_Roupa
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private kostky[] kostka = new kostky[6];
-        
 
         public MainWindow()
         {
-            platno = new Canvas();
             InitializeComponent();
-
             for (int i = 0; i < kostka.Length; i++)
             {
                 kostka[i] = new kostky();
             }
             vykreslitkostky();
-            
-            
         }
 
         private void vykreslitkostku(int x, int y, int vzdalenost, int hodnota)
@@ -53,36 +36,79 @@ namespace Kostky_Roupa
             platno.Children.Add(obryskostky);
 
             cisla(x, y, hodnota, vzdalenost);
-
         }
+
         private void cisla(int x, int y, int hodnota, int vzdalenost)
         {
             double cislovelikost = vzdalenost / 5;
             double odsazeni = vzdalenost / 4;
             double vycentrovano = vzdalenost / 2;
-            Point[] pozice = new Point[]
-            {
-                new Point(vycentrovano, vycentrovano),
-                new Point(vycentrovano - odsazeni, vycentrovano - odsazeni),
-                new Point(vycentrovano + odsazeni, vycentrovano + odsazeni),
-                new Point(vycentrovano - odsazeni, vycentrovano + odsazeni),
-                new Point(vycentrovano + odsazeni, vycentrovano - odsazeni),
-                new Point(vycentrovano - odsazeni, vycentrovano),
-                new Point(vycentrovano + odsazeni, vycentrovano)
-            };
+            Point[] pozice = null;
 
-            Ellipse[] cisla = new Ellipse[hodnota];
+            switch (hodnota)
+            {
+                case 1:
+                    pozice = new Point[] { new Point(vycentrovano, vycentrovano) };
+                    break;
+                case 2:
+                    pozice = new Point[]
+                    {
+                        new Point(vycentrovano - odsazeni, vycentrovano - odsazeni),
+                        new Point(vycentrovano + odsazeni, vycentrovano + odsazeni)
+                    };
+                    break;
+                case 3:
+                    pozice = new Point[]
+                    {
+                        new Point(vycentrovano - odsazeni, vycentrovano - odsazeni),
+                        new Point(vycentrovano, vycentrovano),
+                        new Point(vycentrovano + odsazeni, vycentrovano + odsazeni)
+                    };
+                    break;
+                case 4:
+                    pozice = new Point[]
+                    {
+                        new Point(vycentrovano - odsazeni, vycentrovano - odsazeni),
+                        new Point(vycentrovano + odsazeni, vycentrovano - odsazeni),
+                        new Point(vycentrovano - odsazeni, vycentrovano + odsazeni),
+                        new Point(vycentrovano + odsazeni, vycentrovano + odsazeni)
+                    };
+                    break;
+                case 5:
+                    pozice = new Point[]
+                    {
+                        new Point(vycentrovano - odsazeni, vycentrovano - odsazeni),
+                        new Point(vycentrovano + odsazeni, vycentrovano - odsazeni),
+                        new Point(vycentrovano, vycentrovano),
+                        new Point(vycentrovano - odsazeni, vycentrovano + odsazeni),
+                        new Point(vycentrovano + odsazeni, vycentrovano + odsazeni)
+                    };
+                    break;
+                case 6:
+                    pozice = new Point[]
+                    {
+                        new Point(vycentrovano - odsazeni, vycentrovano - odsazeni),
+                        new Point(vycentrovano + odsazeni, vycentrovano - odsazeni),
+                        new Point(vycentrovano - odsazeni, vycentrovano),
+                        new Point(vycentrovano + odsazeni, vycentrovano),
+                        new Point(vycentrovano - odsazeni, vycentrovano + odsazeni),
+                        new Point(vycentrovano + odsazeni, vycentrovano + odsazeni)
+                    };
+                    break;
+            }
+
+            Ellipse[] tecky = new Ellipse[hodnota];
             for (int i = 0; i < hodnota; i++)
             {
-                cisla[i] = new Ellipse
+                tecky[i] = new Ellipse
                 {
                     Width = cislovelikost,
                     Height = cislovelikost,
                     Fill = Brushes.Black
                 };
-                Canvas.SetTop(cisla[i], y + pozice[i].Y - cislovelikost / 2);
-                Canvas.SetLeft(cisla[i], x + pozice[i].X - cislovelikost / 2);
-                platno.Children.Add(cisla[i]);
+                Canvas.SetTop(tecky[i], y + pozice[i].Y - cislovelikost / 2);
+                Canvas.SetLeft(tecky[i], x + pozice[i].X - cislovelikost / 2);
+                platno.Children.Add(tecky[i]);
             }
         }
 
@@ -100,21 +126,22 @@ namespace Kostky_Roupa
         {
             for (int i = 0; i < 20; i++)
             {
-                foreach (var kostky in kostka)
+                foreach (var k in kostka)
                 {
-                    kostky.Hod();
+                    k.Hod();
                 }
                 vykreslitkostky();
                 await Task.Delay(100);
-
             }
         }
+
         private async void btnhod_Click(object sender, RoutedEventArgs e)
         {
             await animacehodu();
             vykreslitkostky();
         }
     }
+
     public class kostky
     {
         private static Random random = new Random();
